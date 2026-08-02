e92e8bebe949f7e21fb6f4136e19c3675e0fa1f7
mic-nation <olengechirac@gmail.com>
2026-08-02 17:12:34 +0100

    Update animation.js

--- docs/js/animation.js
+++ docs/js/animation.js
@@ -1,1 +1,23 @@
-chrfv
+/*==================================
+  HEADER AU DÉFILEMENT
+==================================*/
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 80) {
+
+        header.style.background = "rgba(0,0,0,.90)";
+        header.style.padding = "10px 6%";
+        header.style.boxShadow = "0 8px 25px rgba(0,0,0,.35)";
+
+    } else {
+
+        header.style.background = "rgba(0,0,0,.65)";
+        header.style.padding = "15px 6%";
+        header.style.boxShadow = "none";
+
+    }
+
+});
