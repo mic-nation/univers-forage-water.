ba8c7889671cc121a9699c94654208ffaf31f630
mic-nation <olengechirac@gmail.com>
2026-08-02 17:12:05 +0100

    Update menu.js

--- docs/js/menu.js
+++ docs/js/menu.js
@@ -1,1 +1,27 @@
-menu 
+/*==================================
+  MENU HAMBURGER
+==================================*/
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navbar = document.querySelector(".navbar");
+
+menuToggle.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+
+    // Change l'icône
+    if (navbar.classList.contains("active")) {
+        menuToggle.innerHTML = "✖";
+    } else {
+        menuToggle.innerHTML = "☰";
+    }
+});
+
+// Ferme le menu lorsqu'on clique sur un lien
+const links = document.querySelectorAll(".navbar a");
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        navbar.classList.remove("active");
+        menuToggle.innerHTML = "☰";
+    });
+});
